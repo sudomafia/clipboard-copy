4e87ad068f9dd5152b99803a88bc1a7b3ed2e479
Carlos Jerez <jerez@users.noreply.github.com>
2020-03-03 15:37:34 -0500

    Polyfill with a copy listener instead of creating the span component which causes paste issues

--- index.js
+++ index.js
@@ -10,39 +10,17 @@ function clipboardCopy (text) {
       throw (err !== undefined ? err : new DOMException('The request is not allowed', 'NotAllowedError'))
     })
   }
-
-  // ...Otherwise, use document.execCommand() fallback
-
-  // Put the text to copy into a <span>
-  var span = document.createElement('span')
-  span.textContent = text
-
-  // Preserve consecutive spaces and newlines
-  span.style.whiteSpace = 'pre'
-
-  // Add the <span> to the page
-  document.body.appendChild(span)
-
-  // Make a selection object representing the range of text selected by the user
-  var selection = window.getSelection()
-  var range = window.document.createRange()
-  selection.removeAllRanges()
-  range.selectNode(span)
-  selection.addRange(range)
-
-  // Copy text to the clipboard
-  var success = false
-  try {
-    success = window.document.execCommand('copy')
-  } catch (err) {
-    console.log('error', err)
-  }
-
-  // Cleanup
-  selection.removeAllRanges()
-  window.document.body.removeChild(span)
-
-  return success
-    ? Promise.resolve()
-    : Promise.reject(new DOMException('The request is not allowed', 'NotAllowedError'))
+  // ...Otherwise, use this polyfill logic
+  return new Promise(function(resolve, reject) {
+    var success = false;
+    function listener(e) {
+      e.clipboardData.setData("text/plain", str);
+      e.preventDefault();
+      success = true;
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+    success ? resolve(): reject(new DOMException('The request is not allowed', 'NotAllowedError'));
+  });
 }
